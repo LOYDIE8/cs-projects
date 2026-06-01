d716a243ba15d81decd66d20bad3bb008ae88509
LOYDIE8 <capecenio3@gmail.com>
2026-06-01 22:47:15 +0800

    ToDoList_DONE

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq.Expressions;
+using System.Net.NetworkInformation;
+using System.Runtime.Intrinsics.Arm;
 
 namespace ToDo
 {
@@ -7,21 +10,79 @@ namespace ToDo
         public class Task
         {
         public String Goal { get; set;}
-        public String Status { get; set;}
+        public String IsDone { get; set;}
 
-        public Task(String goal, String status)
+        public Task(String goal, String isDone)
             {
                 Goal = goal;
-                Status = status;
+                IsDone = isDone;
             }
         }
-
         public static void Main(String[] args)
         {
             List<Task> tasks = new List<Task>();
-
-            tasks.Add(new Task("clean house", "not done"));
-            System.Console.WriteLine($"{tasks[0].Goal} {tasks[0].Status} ");
+            int loop = 0;
+            System.Console.WriteLine("Welcome to our to do list bullshit");
+            while(loop == 0){
+                System.Console.WriteLine("please choose an action:");
+                System.Console.WriteLine("[1] Show Tasks\n[2] Add Tasks\n[3] Change Task Status\n[4] Delete tasks\n[5] Exit");
+                int action;
+                if (!int.TryParse(Console.ReadLine(), out action))
+                {
+                    System.Console.WriteLine("INVALID ACTION");
+                }
+                switch (action)
+                {
+                    case 1:
+                    System.Console.WriteLine("====TASKS==========STATUS====");
+                    foreach(Task task in tasks)
+                        {
+                            System.Console.WriteLine($"{task.Goal}              {task.IsDone}");
+                        }
+                    System.Console.WriteLine("\n\n");
+                    break;
+                    case 2:
+                    System.Console.WriteLine("PLEASE ENTER TASK NAME");
+                    tasks.Add(new Task(Console.ReadLine() ?? "error", "not done"));
+                    break;
+                    case 3:
+                     System.Console.WriteLine("==#====TASKS==========STATUS====");
+                     int i = 0;
+                    foreach(Task task in tasks)
+                        {
+                            System.Console.WriteLine($"[{i}]    {task.Goal}              {task.IsDone}");
+                            i++;
+                        }
+                    System.Console.WriteLine("PLEASE CHOOSE WHAT TASK TO CHANGE STATUS");
+                    int index;
+                    if(!int.TryParse(Console.ReadLine(), out index)) { Console.WriteLine("invalid number bobo"); break; }
+                    if(index > tasks.Count){System.Console.WriteLine("wala naman sa choices yan eh bobo"); break;}
+                    System.Console.WriteLine("Please enter new status");
+                    tasks[index].IsDone = Console.ReadLine()?? "not done";
+                    break;
+                    case 4:
+                    System.Console.WriteLine("==#====TASKS==========STATUS====");
+                    i = 0;
+                    foreach(Task task in tasks)
+                        {
+                            
+                            System.Console.WriteLine($"[{i}]    {task.Goal}              {task.IsDone}");
+                            i++;
+                        }
+                    System.Console.WriteLine("PLEASE CHOOSE WHAT TASK TO REMOVE");
+                    if(!int.TryParse(Console.ReadLine(), out index)) { Console.WriteLine("invalid number bobo"); break; }
+                    if(index > tasks.Count){System.Console.WriteLine("wala naman sa choices yan eh bobo"); break;}
+                    tasks.RemoveAt(index);
+                    break;
+                    case 5:
+                    loop = 1;
+                    break;
+                    default:
+                    System.Console.WriteLine("Error");
+                    loop = 1;
+                    break;
+                }
+            }
         }
     }
 }
--- ToDoList/obj/Debug/net10.0/ToDoList.AssemblyInfo.cs
+++ ToDoList/obj/Debug/net10.0/ToDoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a73ee34409b6819c929fb12c14a9a8a308813cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
