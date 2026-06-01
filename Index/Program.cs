6a73ee34409b6819c929fb12c14a9a8a308813cd
LOYDIE8 <capecenio3@gmail.com>
2026-06-01 20:45:05 +0800

    initial commit

---
+++ Index/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Index
+{
+    class Calculator
+    {
+            static void UseCalc()
+            {
+                int num1, num2;
+                Console.WriteLine("\nEnter first number: ");
+                if(!int.TryParse(Console.ReadLine(), out num1))
+                        {System.Console.WriteLine("ERROR you that is not a number!");
+                        return;}
+                Console.WriteLine("Choose action: +, -, *, / ");
+                        char action = Console.ReadKey().KeyChar;
+                Console.WriteLine("\nEnter second number: ");
+                if(!int.TryParse(Console.ReadLine(), out num2))
+                        {System.Console.WriteLine("ERROR you that is not a number!");
+                        return;}  
+                if(action == '/' || num2 == 0){System.Console.WriteLine("error you cannot devide with 0"); return;}  
+                int result = action switch
+                {
+                    '+' => (num1 + num2),
+                    '-' => (num1 - num2),
+                    '*' => (num1 * num2),
+                    '/' when num2 != 0 => (num1 / num2),
+                };
+                System.Console.WriteLine($"{num1} {action} {num2} = {result}");
+                return;
+            }
+            static void Main(String[] args)
+            {
+                System.Console.WriteLine("wanna use calc? y/n");
+                char calc = Console.ReadKey().KeyChar;
+                while(calc == 'y')
+            {
+                UseCalc();
+                System.Console.WriteLine("Continue using calc? y/n");
+                calc = Console.ReadKey().KeyChar;
+            }
+            }
+    }
+}
---
+++ ToDoList/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ToDo
+{
+    class ToDoList
+    {
+        public class Task
+        {
+        public String Goal { get; set;}
+        public String Status { get; set;}
+
+        public Task(String goal, String status)
+            {
+                Goal = goal;
+                Status = status;
+            }
+        }
+
+        public static void Main(String[] args)
+        {
+            List<Task> tasks = new List<Task>();
+
+            tasks.Add(new Task("clean house", "not done"));
+            System.Console.WriteLine($"{tasks[0].Goal} {tasks[0].Status} ");
+        }
+    }
+}
